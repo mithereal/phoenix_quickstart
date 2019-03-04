0223131336314b99bea3a8c92d6ca3dee7044cfb
Jason Clark <mithereal@gmail.com>
2019-03-04 15:51:55 -0700

    .

--- .iex.exs
+++ .iex.exs
@@ -1,29 +1,5 @@
 alias DB.Repo
-alias DB.Account
-alias DB.Company
-alias DB.CrashDump
-alias DB.Credits
-alias DB.Debits
-alias DB.Item
-alias DB.Rate
-alias DB.Room
 alias DB.User
-alias DB.Image
-alias DB.ServiceArea
-alias DB.Zipcode
-alias DB.USPostalCode
-
-alias Bank.Application, as: Bank
-alias USPostOffice.Application, as: USPS
-
-alias Companies.Company.Client, as: CC
-alias Companies.Company.Server, as: CS
-alias Companies.Company.Supervisor, as: CSS
-alias ServiceAreas.ServiceArea.Server, as: SA
-alias ServiceAreas.ServiceArea.Supervisor, as: SAS
-alias Leads.Lead.Server, as: LS
-alias Leads.Lead.Supervisor, as: LSS
-alias Leads.Lead.Dispatcher, as: LD
-alias Leads.Quote.Presenter.Server, as: PS
+alias DB.User.Profile
 
 alias :observer, as: O
