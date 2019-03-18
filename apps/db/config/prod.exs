9177fc323490a8326d8ee1c82efa363d77f8d640
Jason Clark <mithereal@gmail.com>
2019-03-18 11:11:24 -0700

    .

--- apps/db/config/prod.exs
+++ apps/db/config/prod.exs
@@ -1,3 +1,11 @@
 use Mix.Config
 
+config :db, DB.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "public",
+  hostname: "localhost",
+  pool_size: 10
+
 import_config "prod.secret.exs"
